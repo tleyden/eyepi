b24b9e8c61b59f590b8abbf10afe815b36588ac3
Traun Leyden <traun.leyden@gmail.com>
2021-01-03 09:00:18 -0800

    re-add default res

--- src/eyepi.py
+++ src/eyepi.py
@@ -395,7 +395,7 @@ if __name__ == "__main__":
     parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                         default=0.5)
     parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                        default='')
+                        default='1280x720')
     parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                         action='store_true')
 
