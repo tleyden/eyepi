da3942997837bd3e9a3a40a5cfe6a7176733149d
Traun Leyden <traun.leyden@gmail.com>
2021-01-10 22:45:16 -0800

    Don't draw framerate on image

--- src/eyepi.py
+++ src/eyepi.py
@@ -176,8 +176,6 @@ def main(args):
                 cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
                 cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
-        # Draw framerate in corner of frame
-        cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
         print("Frame rate: {}".format(frame_rate_calc))
 
         # Process EyePiDetectionEvent
