f8ccf43f16b86af2be9fe7792ac61b38d4ea0f58
Traun Leyden <traun.leyden@gmail.com>
2021-01-10 17:48:27 -0800

    Reuse push_event_to_s3

--- src/eyepi.py
+++ src/eyepi.py
@@ -534,54 +534,15 @@ class EyePiEventStream(object):
         self.num_captured_frames = 0
 
     def push_event_to_s3(self, event, filename, object_name):
-        """
-        - Push video to s3
-        - Generate signed URL for video
-        - Write an alert file that says "Person detected .. <link to video>"
-        - Write alert file to s3
-        """
 
-        try:
-            print("Uploading {} -> {}/{} .. ".format(filename, self.bucket_name, object_name))
-            response = self.s3_client.upload_file(
-                filename,
-                self.bucket_name,
-                object_name,
-            )
-            print("Finished uploading {} -> {}/{} .. ".format(filename, self.bucket_name, object_name))
-
-            # Make the video capture file public
-            # TODO: use signed URLs instead of making the file public
-            self.s3_client.put_object_acl(ACL='public-read', Bucket=self.bucket_name, Key="%s" % (object_name))
-
-
-            # Create and upload alert meta file
-            public_url = f'https://{self.bucket_name}.s3.amazonaws.com/{object_name}'
-
-            alert_meta = {
-                "detected_object": self.target_object,
-                "detection_confidence": float(self.last_object_detected_confidence),
-                "captured_video_url": public_url,
-            }
-
-            alert_meta_object_name = "{}.json".format(object_name)
-            alert_meta_filepath = "/tmp/{}".format(alert_meta_object_name)
-            f = open(alert_meta_filepath, "a")
-            f.write(json.dumps(alert_meta))
-            f.close()
-
-            print("Uploading {} -> {}/{} .. ".format(alert_meta_filepath, self.bucket_name, alert_meta_object_name))
-
-            response = self.s3_client.upload_file(
-                alert_meta_filepath,
-                self.bucket_name,
-                alert_meta_object_name,
-            )
-            print("Finished uploading {} -> {}/{} .. ".format(alert_meta_filepath, self.bucket_name, alert_meta_object_name))
-
-        except Exception as e:
-            print("Exception writing {} to s3: {}".format(object_name, str(e)))
-            raise e
+        push_event_to_s3(
+            s3_client=self.s3_client,
+            bucket_name=self.bucket_name,
+            filename=filename,
+            object_name=object_name,
+            detected_object=self.target_object,
+            detection_confidence=float(self.last_object_detected_confidence),
+        )
 
 def future_callback_error_logger(future):
     """
